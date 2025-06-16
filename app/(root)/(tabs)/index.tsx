5877a09bacc24f08f979fd2e2b576b851c34a921
GyabaahFelix <felixic360@gmail.com>
2025-06-16 12:29:16 +0000

    first commit on June16

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 30, marginBottom: 20 }}>Welcome to Expo Router!</Text>
+      <Link href="/sign-in">SignIn</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Properties</Link>
+    </View>
+  );
+}
